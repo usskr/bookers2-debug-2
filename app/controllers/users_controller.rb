4981c732f2ac66cac623dc0d1e61495291ef96ea
usskr <rei.takagi731@gmail.com>
2021-09-02 12:44:25 +0000

    [Fix] top画面の表示まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
     @user = User.find(params[:id])
@@ -24,6 +25,7 @@ class UsersController < ApplicationController
   end
 
   private
+  
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
@@ -34,4 +36,5 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
+  
 end
